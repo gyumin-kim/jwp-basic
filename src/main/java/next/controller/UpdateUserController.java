9032a4a17487d66d783bd9ef542859e4466661a5
Gyumin Kim <starkying@gmail.com>
2020-07-27 06:20:17 +0900

    7.1 회원 데이터를 DB에 저장하기
    * `UserDao`: `update`, `findAll` 구현
    * controller에서 `DataBase` 대신 `UserDao` 사용

--- src/main/java/next/controller/UpdateUserController.java
+++ src/main/java/next/controller/UpdateUserController.java
@@ -1,21 +1,22 @@
 package next.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import core.mvc.Controller;
+import next.dao.UserDao;
+import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import core.db.DataBase;
-import core.mvc.Controller;
-import next.model.User;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class UpdateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(UpdateUserController.class);
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        User user = DataBase.findUserById(req.getParameter("userId"));
+        String userId = req.getParameter("userId");
+        UserDao userDao = new UserDao();
+        User user = userDao.findByUserId(userId);
         if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
             throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
         }
