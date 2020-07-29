ab11ef8bdd4ac8e7e45cf93df7b7b18350cb4307
Gyumin Kim <starkying@gmail.com>
2020-07-29 22:55:09 +0900

    7.2 DAO 리팩토링
    * `JdbcTemplate`: `User`에 대한 의존관계 제거

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -1,32 +1,18 @@
 package core.jdbc;
 
-import next.model.User;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public abstract class JdbcTemplate {
 
-	public void update(final User user) throws SQLException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			con = ConnectionManager.getConnection();
-			String sql = createQuery();
-			pstmt = con.prepareStatement(sql);
-			setValues(user, pstmt);
+	public void update(String sql) throws SQLException {
+		try (Connection con = ConnectionManager.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(sql)) {
+			setValues(pstmt);
 			pstmt.executeUpdate();
-		} finally {
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (con != null) {
-				con.close();
-			}
 		}
 	}
 
-	public abstract String createQuery();
-	public abstract void setValues(final User user, final PreparedStatement pstmt) throws SQLException;
+	public abstract void setValues(final PreparedStatement pstmt) throws SQLException;
 }
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -15,37 +15,29 @@ public class UserDao {
     public void insert(User user) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate() {
             @Override
-            public String createQuery() {
-                return "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            }
-
-            @Override
-            public void setValues(final User user, final PreparedStatement pstmt) throws SQLException {
+            public void setValues(final PreparedStatement pstmt) throws SQLException {
                 pstmt.setString(1, user.getUserId());
                 pstmt.setString(2, user.getPassword());
                 pstmt.setString(3, user.getName());
                 pstmt.setString(4, user.getEmail());
             }
         };
-        jdbcTemplate.update(user);
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql);
     }
 
     public void update(User user) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate() {
             @Override
-            public String createQuery() {
-                return "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
-            }
-
-            @Override
-            public void setValues(final User user, final PreparedStatement pstmt) throws SQLException {
+            public void setValues(final PreparedStatement pstmt) throws SQLException {
                 pstmt.setString(1, user.getPassword());
                 pstmt.setString(2, user.getName());
                 pstmt.setString(3, user.getEmail());
                 pstmt.setString(4, user.getUserId());
             }
         };
-        jdbcTemplate.update(user);
+        String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
+        jdbcTemplate.update(sql);
     }
 
     public List<User> findAll() throws SQLException {
